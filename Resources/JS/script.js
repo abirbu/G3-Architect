bfbbe68d5eddfdb8b7875c5ca63306deef49d7dd
abirbu <ariful.islam2874@gmail.com>
2023-01-24 20:11:32 +0600

    navbar added and responsive

---
+++ Resources/JS/script.js
@@ -0,0 +1,14 @@
+function openMenu(){
+    document.getElementById('ham-menu').style.display= "block";
+}
+
+function closeMenu(){
+    document.getElementById('ham-menu').style.display= "none";
+}
+
+function hideHam(){
+    document.getElementById('ham-icon').style.display = "none";
+}
+function showHam(){
+    document.getElementById('ham-icon').style.display = "block";
+}
